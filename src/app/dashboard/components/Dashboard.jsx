3300769e5714209e5612ec2e8850463833dc033e
Nary Lozano <narylozano@gmail.com>
2024-11-14 15:16:39 +0100

    First v of overview page

---
+++ src/app/dashboard/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Overview from './overview/Overview'
+import SidebarDashboard from './sidebar/Sidebar'
+
+function Dashboard () {
+  return (
+    <div className='flex h-full'>
+      <SidebarDashboard />
+      <Overview />
+    </div>
+  )
+}
+export default Dashboard
