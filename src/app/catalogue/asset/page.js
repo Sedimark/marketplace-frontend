b5882658aa195baaeb66c2dbd3c84cb3b297955e
Nary Lozano <narylozano@gmail.com>
2024-11-14 16:03:04 +0100

    Add page and fix styles

--- src/app/catalogue/asset/page.js
+++ src/app/catalogue/asset/page.js
@@ -7,8 +7,8 @@ export default function Page () {
     <>
       <div className='bg-sedimark-light-blue pt-2 pb-2'>
         <Button className=' max-w-fit m-2 ml-10 bg-sedimark-light-gray enabled:hover:bg-sedimark-medium-gray text-sedimark-dark-gray' href='/catalogue'>
-          <svg class='w-5 h-5 text-sedimark-dark-deep-blue dark:text-white' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
-            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 12h14M5 12l4-4m-4 4 4 4' />
+          <svg className='w-5 h-5 text-sedimark-dark-deep-blue dark:text-white' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
+            <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M5 12h14M5 12l4-4m-4 4 4 4' />
           </svg>
           Back to search
         </Button>
--- src/app/dashboard/components/Dashboard.jsx
+++ src/app/dashboard/components/Dashboard.jsx
@@ -1,13 +1,21 @@
 'use client'
 
+<<<<<<< Updated upstream
 import Overview from './overview/Overview'
+=======
+// import Overview from '../overview/components/Overview'
+>>>>>>> Stashed changes
 import SidebarDashboard from './sidebar/Sidebar'
 
 function Dashboard () {
   return (
     <div className='flex h-full'>
       <SidebarDashboard />
+<<<<<<< Updated upstream
       <Overview />
+=======
+      {/* <Overview /> */}
+>>>>>>> Stashed changes
     </div>
   )
 }
