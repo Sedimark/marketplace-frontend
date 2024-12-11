4a32fcef84b81bcd80ab887a16f971530e59df65
Maxime Costalonga <maxime.costalonga@gmail.com>
2024-12-11 23:11:53 +0000

    fix: remove federated catalogue unused env variables

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -3,9 +3,6 @@
  *
  * @typedef {Object} Settings
  * @property {number} batchSize - The total of items per page.
- * @property {URL} federatedCatalogueApi - The URL for the federated catalogue API.
- * @property {URL} federatedCatalogueProviders - The URL for the federated catalogue providers API.
- * @property {URL} federatedCatalogueFilters - The URL for the federated catalogue filters API.
  */
 
 /**
@@ -14,10 +11,7 @@
  * @type {Settings}
  */
 const settings = {
-  batchSize: process.env.BATCH_SIZE ?? 40,
-  federatedCatalogueApi: new URL('/query_page', process.env.FEDERATED_CATALOGUE_API),
-  federatedCatalogueProviders: new URL('/api/providers', process.env.FEDERATED_CATALOGUE_API),
-  federatedCatalogueFilters: new URL('/api/filters-data', process.env.FEDERATED_CATALOGUE_API)
+  batchSize: process.env.BATCH_SIZE ?? 40
 }
 Object.freeze(settings)
 
