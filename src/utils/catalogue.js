9c232c4b1340c9f24412cb1f1cc4ebdef3675a76
Maxime Costalonga <maxime.costalonga@gmail.com>
2025-02-03 22:36:01 +0000

    feat: fetch all offerings if no query provided

--- src/utils/catalogue.js
+++ src/utils/catalogue.js
@@ -5,16 +5,16 @@ export function getSparQLQueryString (query, currentPage, batchSize) {
   const baseString = `
     PREFIX dc: <http://purl.org/dc/elements/1.1/>
     PREFIX http: <http://www.w3.org/2011/http#>
-    prefix sedi: <https://w3id.org/sedimark/ontology#>
-    prefix owl: <http://www.w3.org/2002/07/owl#>
-    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    prefix xml: <http://www.w3.org/XML/1998/namespace>
-    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    prefix dcat: <http://www.w3.org/ns/dcat#>
-    prefix dct: <http://purl.org/dc/terms/>
-    prefix dce: <http://purl.org/dc/elements/1.1/>
-    prefix vocab: <https://w3id.org/sedimark/vocab#>
+    PREFIX sedi: <https://w3id.org/sedimark/ontology#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX xml: <http://www.w3.org/XML/1998/namespace>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dcat: <http://www.w3.org/ns/dcat#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX dce: <http://purl.org/dc/elements/1.1/>
+    PREFIX vocab: <https://w3id.org/sedimark/vocab#>
 
     SELECT DISTINCT ?offering ?asset ?title ?description ?publisher ?created ?dataset
     WHERE {
@@ -25,7 +25,9 @@ export function getSparQLQueryString (query, currentPage, batchSize) {
       ?offering dct:publisher ?publisher .
       ?offering dct:created ?created .
       ?offering dcat:dataset ?dataset .
-      FILTER(contains(str(?title), "${query}") || contains(str(?description), "${query}"))
+      FILTER(
+        (contains(str(?title), "${query}") || contains(str(?description), "${query}"))
+      )
     }
     ORDER BY ?created
     LIMIT ${batchSize}
