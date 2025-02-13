381204e3b8d64bffc1896e12ff64c3ce74b48190
Alvaro Fernandez Bejarano <alvarohk_piscis@hotmail.com>
2025-02-13 11:59:33 +0100

    Default value for onClick

--- src/app/publish/components/PublishForm.jsx
+++ src/app/publish/components/PublishForm.jsx
@@ -88,7 +88,7 @@ export default function PublishForm () {
       <Card className=' flex items-center w-1/2 mt-8'>
         <p>Do you wish to reuse an existing asset ?</p>
         <div className='flex flex-row'>
-          <Dropdown label='Select Existing' dismissOnClick={true} className='focus:ring-0'>
+          <Dropdown label='Select Existing' className='focus:ring-0'>
             {existingAssets.map((asset, index) => (
               <Dropdown.Item className='focus:ring-0' key={index} onClick={() => handleSelectExisting(asset)}>{asset.title}</Dropdown.Item>
             ))}
