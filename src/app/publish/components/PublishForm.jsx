5de4894e4dd7ade9c28d73ea367109d8738cd7dd
Nary Lozano <63013781+NaryLozano@users.noreply.github.com>
2025-02-19 10:41:56 +0100

    Refactor: Publish Form to Accordions (#34)
    * Wip 01.27.25/13:14
    
    * Dynamic prefill
    
    * Refactor publish form, merge steps in one file + add review modal
    
    * Update Docstrings
    
    * Fixed triggering Formik onSubmit logic + modal added to it
    
    * UI imporvements for PR
    
    * removed steps vars
    
    * Default value for onClick
    
    ---------
    
    Co-authored-by: Alvaro Fernandez Bejarano <alvarohk_piscis@hotmail.com>

--- src/app/publish/components/PublishForm.jsx
+++ src/app/publish/components/PublishForm.jsx
@@ -1,82 +1,146 @@
 'use client'
 import { useState } from 'react'
-import { Card } from 'flowbite-react'
-import AssetDefinition from './steps/AssetDefinition'
-import Access from './steps/Access'
-import Policy from './steps/Policy'
-import { initialValuesAccessEmpty, initialValuesAssetDefinitionEmpty, initialValuesPolicyEmpty } from './steps/initialValues'
+import { Button, Card, Dropdown, Modal, Popover } from 'flowbite-react'
+import mockExistingAssets from '@/utils/data/mockExistingAssets.json'
+import AssetForm from './assetDefinition/AssetForm'
 
 /**
- * FormSteps component renders a multi-step form with navigation controls.
+ * PublishForm component for creating or reusing assets.
  *
- * @component
- * @example
- * return (
- *   <FormSteps />
- * )
+ * This component provides a multi-step form for defining and publishing assets.
+ * Users can choose to create a new asset or reuse an existing one.  The form
+ * guides users through defining the asset's properties, access permissions,
+ * pricing and policies, and finally, reviewing and submitting the information.
  *
- * @returns {JSX.Element} The rendered component.
- *
- * @description
- * This component manages the state of the active step in the form and provides
- * navigation controls to move between steps. It renders a stepper to indicate
- * the current step and conditionally renders the content of each step based on
- * the active step.
- *
- * @function
- * @name FormSteps
- *
- * @property {number} activeStep - The current active step in the form.
- * @property {function} setActiveStep - Function to update the active step.
- * @property {function} handleNext - Function to move to the next step.
- * @property {function} handlePrev - Function to move to the previous step.
- * @property {Array<string>} steps - Array of step names.
- * @property {object} initialValuesStepOne - Initial values for step one.
- * @property {function} setInitialValuesStepOne - Function to update initial values for step one.
- * @property {object} initialValuesStepTwo - Initial values for step two.
- * @property {function} setInitialValuesStepTwo - Function to update initial values for step two.
- * @property {object} initialValuesStepThree - Initial values for step three.
- * @property {function} setInitialValuesStepThree - Function to update initial values for step three.
+ * @returns {JSX.Element} The PublishForm component.
  */
-export default function FormSteps () {
-  const [activeStep, setActiveStep] = useState(0)
+export default function PublishForm () {
+  const initialValuesEmpty = {
+    title: '',
+    description: '',
+    image: '',
+    keywords: [],
+    url: '',
+    url_action: 'GET',
+    headers: [{ key: '', value: '' }],
+    queries: [{
+      name: '',
+      label: '',
+      description: '',
+      type: 'text',
+      default_value: '',
+      required: true
+    }],
+    license: '',
+    terms_and_condition: '',
+    data_controller: '',
+    legal_basis: '',
+    purpose: '',
+    data_protection_contract_point: '',
+    consent_withdrawal_contact_point: '',
+    switchQuery: false,
+    switchPII: false,
+    policies: [{ period: { startDate: '', endDate: '' }, policyName: '' }]
+  }
+  const [openModal, setOpenModal] = useState(false)
+  const [initialValues, setInitialValues] = useState(initialValuesEmpty)
+  const [currentAsset, setCurrentAsset] = useState(null)
+  const existingAssets = mockExistingAssets
 
-  const handleNext = () => setActiveStep((cur) => cur + 1)
-  const handlePrev = () => setActiveStep((cur) => cur - 1)
-
-  const steps = ['Asset Definition', 'Access', 'Pricing & Policies', 'Review & Submit']
-
-  const [initialValuesAssetDefinition, setInitialValuesAssetDefinition] = useState(initialValuesAssetDefinitionEmpty)
-  const [initialValuesAccess, setInitialValuesAccess] = useState(initialValuesAccessEmpty)
-  const [initialValuesPolicy, setInitialValuesPolicy] = useState(initialValuesPolicyEmpty)
+  const handleSelectExisting = (asset) => {
+    setInitialValues({
+      title: asset.title,
+      description: asset.description,
+      image: asset.image,
+      keywords: asset.keywords,
+      url: asset.endpointURL,
+      url_action: 'GET',
+      headers: [{ key: 'hello', value: 'world' }],
+      queries: [{
+        name: '',
+        label: '',
+        description: asset.endpointDescription,
+        type: 'text',
+        default_value: '',
+        required: true
+      }],
+      license: asset.license,
+      terms_and_condition: '',
+      data_controller: '',
+      legal_basis: '',
+      purpose: '',
+      data_protection_contract_point: '',
+      consent_withdrawal_contact_point: '',
+      switchQuery: false,
+      switchPII: false,
+      policies: [{ period: { startDate: '', endDate: '' }, policyName: '' }]
+    }
+    )
+    setCurrentAsset(asset.id)
+  }
+  const handleNewAsset = () => {
+    setInitialValues(initialValuesEmpty)
+    setCurrentAsset(`Empty + ${Date.now()}`)
+  }
 
   return (
     <div className='flex flex-col items-center justify-center mt-4'>
-      {/* Stepper */}
-      <Card className='items-center w-1/2 mt-8'>
-        <ol className='items-center w-full space-y-4 xl:flex xl:space-x-8 xl:space-y-0 rtl:space-x-reverse'>
-          {steps.map((_step, index) => (
-            <li key={index} className={'flex items-center space-x-2.5 rtl:space-x-reverse ' + (activeStep === index ? 'text-blue-600' : 'text-gray-500')}>
-              <span className={'flex items-center justify-center w-6 h-6 border border-blue-600 rounded-full shrink-0 ' + (activeStep === index ? 'border-blue-600' : 'border-gray-500')}>
-                {index + 1}
-              </span>
-              <span className='pr-2'>
-                <h3 className='font-medium leading-tight'>{_step}</h3>
-              </span>
-              {(index + 1) !== steps.length &&
-                <svg className='w-5 h-5 ms-2 xl:ms-4 rtl:rotate-180' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 12 10'>
-                  <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m7 9 4-4-4-4M1 9l4-4-4-4' />
-                </svg>}
-            </li>
-          ))}
-        </ol>
+      <Card className=' flex items-center w-1/2 mt-8'>
+        <p>Do you wish to reuse an existing asset ?</p>
+        <div className='flex flex-row'>
+          <Dropdown label='Select Existing' className='focus:ring-0'>
+            {existingAssets.map((asset, index) => (
+              <Dropdown.Item className='focus:ring-0' key={index} onClick={() => handleSelectExisting(asset)}>{asset.title}</Dropdown.Item>
+            ))}
+          </Dropdown>
+          <Button className='ml-6 focus:ring-0' onClick={() => handleNewAsset()}>Create New</Button>
+        </div>
       </Card>
-      {/* --------------- Step 1 ---------------  */}
-      {activeStep === 0 && AssetDefinition(initialValuesAssetDefinition, setInitialValuesAssetDefinition, handleNext)}
-      {/* --------------- Step 2 ---------------  */}
-      {activeStep === 1 && Access(initialValuesAccess, setInitialValuesAccess, handleNext, handlePrev)}
-      {/* --------------- Step 3 ---------------  */}
-      {activeStep === 2 && Policy(initialValuesPolicy, setInitialValuesPolicy, handleNext, handlePrev)}
+      {currentAsset &&
+        <div className='items-center w-1/2 mt-8' key={currentAsset}>
+          {AssetForm(initialValues, setInitialValues, openModal, setOpenModal)}
+        </div>}
+
+      {openModal &&
+        <div className='flow-root'>
+          <Modal show={openModal} onClose={() => setOpenModal(false)}>
+            <Modal.Header>Review Asset information:</Modal.Header>
+            <Modal.Body>
+              <pre>
+                {JSON.stringify(initialValues, null, 2)}
+              </pre>
+            </Modal.Body>
+            <Modal.Footer>
+
+              <Popover
+                aria-labelledby='default-popover'
+                content={
+                  <div className='w-64 text-sm text-gray-500 dark:text-gray-400'>
+                    <div className='px-3 py-2'>
+                      <p>To close use the X</p>
+                    </div>
+                  </div>
+                }
+              >
+                <Button>I accept</Button>
+              </Popover>
+              <Popover
+                aria-labelledby='default-popover'
+                content={
+                  <div className='w-64 text-sm text-gray-500 dark:text-gray-400'>
+                    <div className='px-3 py-2'>
+                      <p>This is not implemented, what is the next step after submit?</p>
+                    </div>
+                  </div>
+                }
+              >
+                <Button color='gray'>
+                  Decline
+                </Button>
+              </Popover>
+            </Modal.Footer>
+          </Modal>
+        </div>}
     </div>
   )
 }
