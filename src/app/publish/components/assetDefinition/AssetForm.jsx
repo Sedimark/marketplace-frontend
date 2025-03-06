479c461af163ca9314ac2eab9bd1b2b8049e6219
AlvaroFernandezBejarano <alvarohk_piscis@hotmail.com>
2025-03-06 17:40:25 +0100

    removed logger

--- src/app/publish/components/assetDefinition/AssetForm.jsx
+++ src/app/publish/components/assetDefinition/AssetForm.jsx
@@ -71,7 +71,6 @@ const validationSchemaAssetDefinition = yup.lazy(values =>
  * @returns {JSX.Element} The AssetForm component.
  */
 export default function AssetForm (initialValues, setInitialValues, openModal, setOpenModal) {
-  console.log(initialValues)
   return (
     <>
       <Formik
