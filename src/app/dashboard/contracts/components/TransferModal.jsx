dc5595b52351a756dd19924a7907b56f617b0221
Alvaro Fernandez Bejarano <144214275+AlvaroFernandezBejarano@users.noreply.github.com>
2025-07-08 12:27:09 +0200

    Pull data transfer on contracts (#75)
    * WIP Connector integration
    
    * Simple View Contracts
    
    * Linting
    
    * Update src/utils/connector.js
    
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>
    
    * Added providerBy Filters + clean up
    
    * removed env requirement on pageSize
    
    * l i n t i n g
    
    * Last fixes
    
    * WIP working calls
    
    * Formik + yup + Input for Data Destination
    
    * Cleaning
    
    * l i n t i n g
    
    * Changed required String
    
    * Update JDocs
    
    * Last nitpicks
    
    * Label nitpick
    
    * Working WIP Flow, still no data
    
    * PULL data on new tab with auth headers
    
    * Dangling console.log
    
    * Modal revamp + Loading status spinner
    
    * last touch
    
    * Removed Proxy Route, Added input read-only on Pull Data
    
    * maint: update flowbite-react to 0.10.2
    
    * maint: improve copy button in transfer modal
    
    * maint:remove copytoclipboard function
    
    * Update src/app/dashboard/contracts/components/TransferModal.jsx
    
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>
    
    * defaulted max retry
    
    ---------
    
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>

--- src/app/dashboard/contracts/components/TransferModal.jsx
+++ src/app/dashboard/contracts/components/TransferModal.jsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { useState } from 'react'
-import { Modal, Button, Label, TextInput } from 'flowbite-react'
-import { HiPlay, HiCloudUpload } from 'react-icons/hi'
+import { ClipboardWithIcon, Modal, Button, Label, TextInput, Spinner } from 'flowbite-react'
+import { HiPlay, HiCloudUpload, HiCloudDownload } from 'react-icons/hi'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as yup from 'yup'
 
 export default function TransferModal ({ contractAgreementId, counterPartyAddress, connectorId }) {
   const [open, setOpen] = useState(false)
   const [message, setMessage] = useState(null)
+  const [loadingPull, setLoadingPull] = useState(false)
+  const [authorization, setAuthorization] = useState('')
+  const [endpoint, setEndpoint] = useState('')
 
   // Validation schema
   const FormSchema = yup.object().shape({
@@ -40,6 +43,34 @@ export default function TransferModal ({ contractAgreementId, counterPartyAddres
     }
   }
 
+  async function pullTransfer (contractAgreementId, counterPartyAddress, connectorId) {
+    setLoadingPull(true)
+    try {
+      const response = await fetch('/api/connector/pullData', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          contractId: contractAgreementId,
+          counterPartyAddress,
+          connectorId
+        })
+      })
+      const { authorization, endpoint } = await response.json()
+      if (!authorization || !endpoint) {
+        console.error('Invalid response: missing authorization or endpoint')
+        return
+      }
+      setAuthorization(authorization)
+      setEndpoint(endpoint)
+    } catch (error) {
+      console.error('Error in pullTransfer:', error)
+    } finally {
+      setLoadingPull(false)
+    }
+  }
+
   const handleSubmit = (values, { setSubmitting }) => {
     setMessage(null)
     try {
@@ -49,9 +80,20 @@ export default function TransferModal ({ contractAgreementId, counterPartyAddres
     }
   }
 
+  const HandlePull = () => {
+    try {
+      pullTransfer(contractAgreementId, counterPartyAddress, connectorId)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   const handleClose = () => {
     setOpen(false)
     setMessage(null)
+    setLoadingPull(false)
+    setAuthorization('')
+    setEndpoint('')
   }
 
   return (
@@ -64,6 +106,7 @@ export default function TransferModal ({ contractAgreementId, counterPartyAddres
         <Modal.Header>Transfer options for {contractAgreementId}</Modal.Header>
         <Modal.Body>
           <div className='space-y-6'>
+            <h3 className='text-lg font-semibold text-gray-700'>Push Data</h3>
             <Formik
               initialValues={{ dataDestination: '' }}
               validationSchema={FormSchema}
@@ -87,9 +130,9 @@ export default function TransferModal ({ contractAgreementId, counterPartyAddres
                     />
                   </div>
 
-                  <Button type='submit' disabled={!isValid || isSubmitting}>
+                  <Button type='submit' className='w-full' disabled={!isValid || isSubmitting}>
                     <HiCloudUpload size={24} className='mr-2' />
-                    {isSubmitting ? 'Pushing data...' : 'Push Transfer'}
+                    {isSubmitting ? 'Pushing Data...' : 'Push Data'}
                   </Button>
                   {message && (
                     <p className='text-sm'>
@@ -99,6 +142,67 @@ export default function TransferModal ({ contractAgreementId, counterPartyAddres
                 </Form>
               )}
             </Formik>
+            <div className='w-full border-t border-gray-300' />
+            <h3 className='text-lg font-semibold text-gray-700'>Pull Data</h3>
+            <Button onClick={HandlePull} className='w-full' disabled={loadingPull}>
+              {loadingPull
+                ? (
+                  <>
+                    <Spinner size='sm' className='mr-2' />
+                    Requesting Pull data...
+                  </>
+                  )
+                : (
+                  <>
+                    <HiCloudDownload size={24} className='mr-2' />
+                    Request Pull Data
+                  </>
+                  )}
+            </Button>
+            {(authorization && endpoint) && (
+              <div className='space-y-4 mt-4'>
+                <div>
+                  <Label htmlFor='endpoint' value='Endpoint URL' />
+                  <div className='w-full'>
+                    <div className='relative'>
+                      <div className='block w-full rounded-lg border border-gray-300 bg-white text-sm text-gray-900 focus-within:ring-2 focus-within:ring-blue-500'>
+                        <input
+                          type='text'
+                          readOnly
+                          id='endpoint'
+                          value={endpoint}
+                          className='bg-transparent border-none focus:ring-0 focus:outline-none px-3 py-2 w-full'
+                        />
+                        <ClipboardWithIcon
+                          className='absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-700 bg-white'
+                          valueToCopy={endpoint}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <Label htmlFor='authorization' value='Authorization Token' />
+                  <div className='w-full'>
+                    <div className='relative'>
+                      <div className='block w-full rounded-lg border border-gray-300 bg-white text-sm text-gray-900 focus-within:ring-2 focus-within:ring-blue-500'>
+                        <input
+                          type='text'
+                          readOnly
+                          id='authorization'
+                          value={authorization}
+                          className='bg-transparent border-none focus:ring-0 focus:outline-none px-3 py-2 w-full'
+                        />
+                        <ClipboardWithIcon
+                          className='absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-700 bg-white'
+                          valueToCopy={authorization}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </Modal.Body>
         <Modal.Footer>
