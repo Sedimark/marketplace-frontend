6d9fe991f3d99838d6abffde4084cb857e62259e
Alvaro Fernandez Bejarano <144214275+AlvaroFernandezBejarano@users.noreply.github.com>
2025-07-14 10:41:32 +0200

    Update src/app/dashboard/overview/components/TransferHistory.jsx
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>

--- src/app/dashboard/overview/components/TransferHistory.jsx
+++ src/app/dashboard/overview/components/TransferHistory.jsx
@@ -43,7 +43,7 @@ function TransferHistory ({ transferHistory }) {
             <div className='flex flex-row gap-2'>
               <svg className='w-6 h-6 text-gray-800 dark:text-white' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='none' viewBox='0 0 24 24'>
                 <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z' />
-              </svg>Transfer History
+              </svg>Recent Transfers
             </div>
           </AccordionTitle>
           {transferHistory.length > 0 && (
