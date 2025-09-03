5411da07b62ffccfd5690e720ee6afab36ba1bf3
Alvaro Fernandez Bejarano <144214275+AlvaroFernandezBejarano@users.noreply.github.com>
2025-09-03 17:27:20 +0200

    81 add a dashboard page to manage published offering (#92)
    * WIP Connector integration
    
    * Simple View Contracts
    
    * Linting
    
    * Update src/utils/connector.js
    
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>
    
    * Added providerBy Filters + clean up
    
    * removed env requirement on pageSize
    
    * l i n t i n g
    
    * Last fixes
    
    * WIP working calls
    
    * Formik + yup + Input for Data Destination
    
    * Cleaning
    
    * l i n t i n g
    
    * Changed required String
    
    * Update JDocs
    
    * Last nitpicks
    
    * Label nitpick
    
    * Working WIP Flow, still no data
    
    * PULL data on new tab with auth headers
    
    * Dangling console.log
    
    * Modal revamp + Loading status spinner
    
    * last touch
    
    * WIP transfer History
    
    * Added conditional rendering
    
    * WIP Offering dashboard
    
    * Delelte functionality, disabled cache, Modal confirmation
    
    * Added conditional rendering on error + moved block to try...catch to evade crash on server side
    
    * better style
    
    * Small details
    
    * Added error message on delete modal
    
    * Fixed description root json
    
    * Updated delete functionality
    
    * Re adding lost condtional render + typos & lil fix
    
    * Expected data changed on listing IDs
    
    * Updated frontend for changes on the Offering Manager, using real pagination now
    
    * l i n t i ng
    
    * Fix + redesigned Asset Item
    
    ---------
    
    Co-authored-by: Maxime Costalonga <maxime.costalonga@gmail.com>

---
+++ src/utils/offeringManager.js
@@ -0,0 +1,128 @@
+import settings from '@/utils/settings'
+import { fetchData } from '@/utils/helpers/fetchData'
+
+/**
+ * Fetch call to obtain ALL the IDs stored on the Offering Manager.
+ * @async
+ * @param {string} contractAgreementIdFilter - Value expected to filter by Contract ID.
+ * @returns An Array of Strings representing the IDs of ALL the offerings on the offering-manager
+ */
+export async function fetchOfferingsIDs () {
+  const url = `${settings.offeringManagerUrl}/offerings`
+  const options = {
+    method: 'GET'
+  }
+  try {
+    const data = await fetchData(url, options).then(response => response.json())
+    const offeringIds = (data['sedi:hasOffering'] || []).map(offering => offering['@id'])
+
+    return offeringIds
+  } catch (error) {
+    // Will be 2 printed errors as there is a console.log on the fetchData helper, but as is server side can help us id the error.
+    console.log('Error on fetchOfferingsIDs!')
+    console.log(error)
+    return { error }
+  }
+}
+
+/**
+ * Fetch call to obtain one offering by its ID.
+ * @async
+ * @param {string} offeringId - URL + ID offer to search for it.
+ * @returns An Array of JSON obj representing the Offering.
+ */
+export async function fetchOffering (offeringId) {
+  const url = `${offeringId}` // This is a URL + ID, as it is returned by the Offering Manager this way!
+  const options = {
+    method: 'GET'
+  }
+  try {
+    const data = await fetchData(url, options).then(response => response.json())
+    return data
+  } catch (error) {
+    // Will be 2 printed errors as there is a console.log on the fetchData helper, but as is server side can help us id the error.
+    console.log('Error on fetchOffering!')
+    console.log(error)
+    return { error }
+  }
+}
+
+/**
+ * Fetch call to delete one offering by its ID.
+ * @async
+ * @param {string} offeringId - URL + ID offer to delete.
+ * @returns An Array of JSON obj representing the Offering.
+ */
+export async function deleteOffering (offeringId) {
+  const extractedId = offeringId.split('/').pop()
+  const url = `${settings.offeringManagerUrl}/offerings/${extractedId}`
+  console.log(url)
+  const options = {
+    method: 'DELETE'
+  }
+  try {
+    const data = await fetchData(url, options)
+    return data
+  } catch (error) {
+    // Will be 2 printed errors as there is a console.log on the fetchData helper, but as is server side can help us id the error.
+    console.log('Error on deleteOffering!')
+    console.log(error)
+    return { error }
+  }
+}
+
+/**
+ * New single fetch with pagination
+ * @async
+ * @param {string} currentPage - Used for pagination.
+ * @returns An Array of JSON obj representing the Offerings.
+ */
+export async function fetchOfferings (currentPage) {
+  const pageSize = parseInt(settings.offeringsPageSize)
+  // Yes, need that -1 on currentPage as they use 0 as page 1
+  const url = `${settings.offeringManagerUrl}/offerings?size=${pageSize}&page=${currentPage - 1}`
+  const options = {
+    method: 'GET'
+  }
+  try {
+    const data = await fetchData(url, options).then(response => response.json())
+    // Return only the offerings array, as we dont use anything from the new response!
+    return data['sedi:hasOffering'] || []
+  } catch (error) {
+    // Will be 2 printed errors as there is a console.log on the fetchData helper, but as is server side can help us id the error.
+    console.log('Error on fetchOfferings!')
+    console.log(error)
+    return { error }
+  }
+}
+
+/**
+ * Multiple fetch call to obtain a set of Offerings, while maintaining a "pagination" structure.
+ * Will simulate the pagination on their side, but it is done here!
+ * --------- DEPRECATED -----------
+ * Offering Manager still has the endpoint to work like this function, so this is commented for preserving.
+ * @async
+ * @param {Array} offeringIds - Array of STRINGS to fetch.
+ * @param {string} currentPage - Used for pagination.
+ * @returns An Array of JSON obj representing the Offerings.
+ */
+// export async function fetchOfferingsCustom (offeringIds, currentPage) {
+//   // Pagination custom
+//   try {
+//     const pageSize = parseInt(settings.offeringsPageSize)
+//     const start = (currentPage - 1) * pageSize
+//     const end = start + pageSize
+//     const paginatedIds = offeringIds.slice(start, end)
+//     const data = await Promise.all(
+//       paginatedIds.map((id) =>
+//         fetchOffering(id)
+//       )
+//     )
+//     return data
+//   } catch (error) {
+//     // Will be 2 printed errors as there is a console.log on the fetchData helper, but as is server side can help us id the error.
+//     console.log('Error on fetchOfferingsCustom!')
+//     console.log(error)
+//     return { error }
+//   }
+// }
