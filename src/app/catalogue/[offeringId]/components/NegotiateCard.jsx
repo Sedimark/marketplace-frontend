9d0465f47c4a5f178cfe019a8e445b201731c9c7
xamcost <maxime.costalonga@gmail.com>
2025-09-22 11:01:36 +0100

    fix: asset url in contract negotiation

--- src/app/catalogue/[offeringId]/components/NegotiateCard.jsx
+++ src/app/catalogue/[offeringId]/components/NegotiateCard.jsx
@@ -26,7 +26,7 @@ function NegotiateCard ({ offering, provider }) {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-          datasetID: offering.offering.value,
+          datasetID: offering.asset.value,
           counterPartyAddress: provider.connector_url
         })
       })
